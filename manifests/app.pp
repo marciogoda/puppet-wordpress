5cbe6f09913830a4fe0bf6eac70e21a5ed1118b8
Marcio Goda <marcio.goda@veinteractive.com>
2016-12-09 00:01:58 +0000

    Fixed app wp_home and wp_siteurl variables

--- manifests/app.pp
+++ manifests/app.pp
@@ -44,5 +44,7 @@ class wordpress::app (
     wp_debug             => $wp_debug,
     wp_debug_log         => $wp_debug_log,
     wp_debug_display     => $wp_debug_display,
+    wp_home              => $wp_home,
+    wp_siteurl           => $wp_siteurl
   }
 }
